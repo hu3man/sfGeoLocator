52211146e76a6a15eff50797fa9a7ce1f51f96fc
hu3man <hkrohman@gmail.com>
2019-04-17 11:44:21 -0700

    Finished app

--- force-app/main/default/aura/AccountSearch/AccountSearchController.js
+++ force-app/main/default/aura/AccountSearch/AccountSearchController.js
@@ -1,5 +1,10 @@
 ({
-    myAction : function(component, event, helper) {
-
+    onInit : function(component, event, helper) {
+        var searchTerm = component.get("v.searchTerm");
+        helper.handleSearch(component, searchTerm);
+    },
+    onSearchTermChange: function(component, event, helper){
+        var searchTerm = component.get("v.searchTerm");
+        helper.handleSearch(component, searchTerm);
     }
 })
